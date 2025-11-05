b1a4cd6ca644d11e6ca815b3c55856928d3e79c5
HelenHan050401 <151028129+HelenHan050401@users.noreply.github.com>
2025-11-05 22:03:14 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,21 @@
 package model;
 
-public class Player {
+
+public enum Player {
+    P1("Player1"), P2("Player2");
+
+    private final String playerName;
+
+    Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public Player getOpponent() {
+        if (this == P1) return P2;
+        else return P1;
+    }
 }
