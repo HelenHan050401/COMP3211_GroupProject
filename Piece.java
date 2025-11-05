948bf183eb7b13de20d62f75dc73d511fd96fd1b
HelenHan050401 <151028129+HelenHan050401@users.noreply.github.com>
2025-11-05 22:18:24 +0800

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,4 +1,41 @@
 package model;
 
+/*
+ * 1. the properties of pieces
+ * 2. the movements
+ * 3. output toString
+ * */
 public class Piece {
+    private final AnimalsWeight weight;
+    private boolean isAlive;
+    private final Player owner; // indicate the owner of the piece
+    private Position position; // current position
+
+    public Piece(AnimalsWeight weight, Player owner, Position startPoint) {
+        this.weight = weight;
+        this.owner = owner;
+        this.position = startPoint;
+        this.isAlive = true;
+    }
+
+    public AnimalsWeight getWeight() {
+        return weight;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void moveTo(Position newPosition) {
+        this.position = newPosition;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    @Override
+    public String toString() {
+        return owner.getPlayerName() + " - " + weight.getName() + ": " + position;
+    }
 }
