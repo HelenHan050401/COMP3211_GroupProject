3f6774a20903aabefc70c7a4cd711ee7dc77264d
HelenHan050401 <151028129+HelenHan050401@users.noreply.github.com>
2025-11-07 23:45:40 +0800

    Update AnimalsWeight.java

--- AnimalsWeight.java
+++ AnimalsWeight.java
@@ -1,22 +1,28 @@
 package model;
 
 public enum AnimalsWeight {
-    RAT(1),
-    CAT(2),
-    DOG(3),
-    WOLF(4),
-    LEOPARD(5),
-    TIGER(6),
-    LION(7),
-    ELEPHANT(8);
+    RAT(1, "rat"),
+    CAT(2, "cat"),
+    DOG(3, "dog"),
+    WOLF(4, "wolf"),
+    LEOPARD(5, "leopard"),
+    TIGER(6, "tiger"),
+    LION(7, "lion"),
+    ELEPHANT(8, "elephant");
 
     private final int weight;
+    private final String name;
 
-    AnimalsWeight(int weight) {
+    AnimalsWeight(int weight, String name) {
         this.weight = weight;
+        this.name = name;
     }
 
     public int getWeight() {
         return weight;
+    }
+
+    public String getName() {
+        return name;
     }
 }
